686fb1d1cc4c86df36e1639d00c7ccb096a1395b
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-01 20:06:36 +0100

    sections array withour keys

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $sections = [
-    'justice' => [
+    [
         'title' => 'Come state implementando la recente decisione della 
             Corte di giustizia dell \'Unione europea (CGUE) relativa al diritto all \'oblio?',
         'paragraphs' => [
@@ -12,7 +12,7 @@ $sections = [
             i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.',
             'Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell \'individuo
              a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.',
-             'Per presentare una richiesta di rimozione, compila questo <a href="#"> modulo web </a>. 
+            'Per presentare una richiesta di rimozione, compila questo <a href="#"> modulo web </a>. 
              Riceverai una risposta automatica che conferma la ricezione della tua richiesta. 
              Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po \' di tempo 
              perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo 
@@ -23,38 +23,39 @@ $sections = [
               posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d \'accordo con la nostra valutazione,
                puoi rivolgerti 
              all \'Autorità garante per la protezione dei dati personali nel tuo paese.',
-             'Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità
+            'Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità
               per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori 
               di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare
               la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.',
-              'Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
+            'Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
               modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei 
               quando cercano la maggior parte dei nomi, 
               non soltanto le pagine che sono state soggette a una rimozione.'
-        ]    
-        ],
+        ]
+    ],
 
-        'privacy' => [
-            'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
-            'paragraphs' => [
-                'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è 
+    [
+        'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+        'paragraphs' => [
+            'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è 
                 imperativo offrire un\'elevata sicurezza
                  e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.',
-                 'Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e 
+            'Siamo costantemente al lavoro per garantire un\'elevata sicurezza, proteggere la tua privacy e 
                  rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni 
                  anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per 
                  mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy 
                  di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto 
                  riguarda le informazioni che condividi con Google, hai il massimo controllo.',
-                 'Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, 
+            'Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, 
                  nel <a href="#"> Centro Google per la sicurezza online. </a>',
-                 'Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
-            ]
-            ],
-            
-        'country' => [
-            'title' => 'Perché il mio account è associato a un paese?',
-            'paragraphs' => [' <ol class="first-ul"> Il tuo account è associato a un paese (o territorio) nei Termini di servizio 
+            'Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.'
+        ]
+    ],
+
+    [
+        'title' => 'Perché il mio account è associato a un paese?',
+        'paragraphs' => [
+            ' <ol class="first-ul"> Il tuo account è associato a un paese (o territorio) nei Termini di servizio 
             per poter stabilire due cose:',
             '<li>   La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. 
             Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li><ol class="second-ul" type="a">',
@@ -63,28 +64,28 @@ $sections = [
             '<li>        Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></ol>',
             '<li>  La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol>',
             'Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.'
-            ]
-        ],
-        
-        'your_country' => [
-            'title' => 'Stabilire il paese associato al tuo account',
-            'paragraphs' => [
-                'Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato.
+        ]
+    ],
+
+    [
+        'title' => 'Stabilire il paese associato al tuo account',
+        'paragraphs' => [
+            'Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato.
                  Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai
                  servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.',
-                 'I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, 
+            'I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, 
                  potrebbe occorrere circa un anno per aggiornare l\'associazione del paese.',
-                 'Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza 
+            'Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza 
                  tra il paese in cui lavori e il paese in cui risiedi, l\'installazione di una rete privata virtuale (VPN) per mascherare
                   il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href="#"> Contattaci </a> se ritieni che il paese associato al 
                   tuo account sia sbagliato.'
-            ]
-            ],
+        ]
+    ],
 
-        'remove_history' => [
-            'title' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
-            'paragraphs' => [
-                'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non
+    [
+        'title' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+        'paragraphs' => [
+            'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non
                  possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente
                   di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href="#"> contattare il webmaster </a> del sito su cui 
                   sono pubblicati i 
@@ -93,13 +94,13 @@ $sections = [
                 <a href="#">  fai clic qui. </a> Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l\'aggiornamento, le informazioni
                   non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre
                    possibile <a href="#"> visitare la nostra pagina di assistenza per avere ulteriori informazioni. </a>'
-            ]
-            ],
-         
-        'keys' => [
-            'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
-            'paragraphs' => [
-                'In alcuni casi sì. Quando fai clic  su un risultato della Ricerca Google, 
+        ]
+    ],
+
+    [
+        'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+        'paragraphs' => [
+            'In alcuni casi sì. Quando fai clic  su un risultato della Ricerca Google, 
                 il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, 
                 o URL, della pagina dei risultati di ricerca sotto forma di<a href="#">  URL referrer </a>. Talvolta, l\'URL della pagina dei risultati di 
                 ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata 
@@ -108,37 +109,39 @@ $sections = [
                  ricerca SSL sono disponibili <a href="#">  qui </a>. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere 
                  disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero 
                  ricevere informazioni relative all\' esatte parole chiave che hanno determinato il clic su un annuncio.'
-            ]
-        ]    
-            ];
+        ]
+    ]
+];
 
 $page_links = [
-    'Introduzione', 'Norme sulla Privacy' , 'Termini di Servizio' , 'Tecnologie' , 'Domande frequenti'
-];   
+    'Introduzione', 'Norme sulla Privacy', 'Termini di Servizio', 'Tecnologie', 'Domande frequenti'
+];
 
-$footer_links = ['Google' , 'Tutto su Google' , 'Pricavy' , 'Termini'];
+$footer_links = ['Google', 'Tutto su Google', 'Pricavy', 'Termini'];
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
+
 <body>
-    <header> 
+    <header>
         <div class="left-header">
             <div class="upper-header">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Google.png" alt="logo" id="logo">
@@ -146,58 +149,62 @@ $footer_links = ['Google' , 'Tutto su Google' , 'Pricavy' , 'Termini'];
             </div>
             <div class="bottom-header">
                 <ul>
-                   <?php foreach($page_links as $link) : ?>
-                   <li><a href="#"> <?php echo $link ?></a></li>
-                   <?php endforeach; ?> 
+                    <?php foreach ($page_links as $link) : ?>
+                        <li><a href="#"> <?php echo $link ?></a></li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
         <div class="right-header">
-        <i class="bi bi-three-dots-vertical"></i>
-        <figure id="profile-picture">
-            <div>S</div>
-        </figure>
+            <i class="bi bi-three-dots-vertical"></i>
+            <figure id="profile-picture">
+                <div>S</div>
+            </figure>
         </div>
-    
-            </header>
-            
-    <main class="container">
-    <?php foreach($sections as $section) : ?>
 
-    <section>
+    </header>
 
-        <h2> <?php echo $section['title'] ?></h2>
+    <main>
+        <div class="container">
 
-                 <?php foreach($section['paragraphs'] as $paragraph) : ?>  
-                    <p> <?php echo $paragraph?></p>
-                 <?php endforeach; ?>
+            <?php foreach ($sections as $section) : ?>
+
+                <section>
+
+                    <h2> <?php echo $section['title'] ?></h2>
+
+                    <?php foreach ($section['paragraphs'] as $paragraph) : ?>
+                        <p> <?php echo $paragraph ?></p>
+                    <?php endforeach; ?>
 
 
-    </section>
+                </section>
 
-    <?php endforeach;?>    
-            </main>
-            
-            <footer >
-                <div class="container">
-                    <div class="flex-cont">
+            <?php endforeach; ?>
+        </div>
+    </main>
 
-                        <div class="footer-left">
-                            <ul>
-                                <?php foreach($footer_links as $footer_link) : ?>
-                                    <li><a href="#"> <?php echo $footer_link ?> </a></li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            </div>
-                            <div class="footer-right">
-                                <select  id="">
-                                    <option value="Italiano">Italiano</option>
-                                </select>
-                            </div>
-                        </div>
+    <footer>
+        <div class="container">
+            <div class="flex-cont">
+
+                <div class="footer-left">
+                    <ul>
+                        <?php foreach ($footer_links as $footer_link) : ?>
+                            <li><a href="#"> <?php echo $footer_link ?> </a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+                <div class="footer-right">
+                    <select id="">
+                        <option value="Italiano">Italiano</option>
+                    </select>
+                </div>
             </div>
+        </div>
 
-   
-            </footer>      
+
+    </footer>
 </body>
+
 </html>
